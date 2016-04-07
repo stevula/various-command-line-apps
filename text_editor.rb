824a48424c0d30686d0a8f29dcd40e1b74f94cd8
stevula <brodericksteven@gmail.com>
2016-04-06 17:00:03 -0700

    Add repl, edit, and print functionality

--- text_editor.rb
+++ text_editor.rb
@@ -1,9 +1,12 @@
 class TextEditor
   def initialize
+    @text = []
     greet
+
     if new_or_load == "N"
       run
     else
+      # load file into memory
       run
     end
   end
@@ -35,6 +38,61 @@ class TextEditor
   end
 
   def run
+    puts
+    puts 'Type "quit" to stop entering text.'
+    puts
+
+    loop do
+      line = gets.chomp
+      break if line == "quit"
+      @text << line
+    end
+
+    print
+    prompt_edit
+  end
+
+  def print
+    puts
+    puts "You wrote:"
+    @text.each_with_index {|sentence, line| puts "#{line+1} #{sentence}"}
+    puts
+  end
+
+  def prompt_edit
+    puts "Would you like to edit a line? (Y/N)"
+    puts
+    response = gets.chomp.upcase
+
+    if response = "Y"
+      line = nil
+      until (1..@text.length).include? line
+        puts
+        puts "Which line would you like to edit?"
+        line = gets.chomp.to_i
+      end
+
+      edit(line)
+    end
+  end
+
+  def edit(line)
+    puts
+    puts "Was:"
+    puts @text[line-1]
+    puts
+
+    puts "Enter new text:"
+    @text[line-1] = gets.chomp
+    puts
+
+    print
+  end
+
+  def save
+  end
+
+  def load
   end
 end
 
